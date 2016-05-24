cd40cf0607ec0f871eec5c11621c35347b7784bb
hyelimirane <hyelimirane@gmail.com>
2016-05-24 11:21:18 +0900

    정산 화면에서 정산액 반올림

--- client/settlements/settlement_doIt.js
+++ client/settlements/settlement_doIt.js
@@ -35,16 +35,6 @@ Template.settlementDoIt.onRendered(function() {
 
             if(!list[i].settlementCompleted){
 
-/*                if(i === 0){
-                    divList += '<div class="col-xs-1">구매일자</div>'
-                        + '<div class="col-xs-2">내용</div>'
-                        + '<div class="col-xs-2">구매금액</div>'
-                        + '<div class="col-xs-1">구매인원</div>'
-                        + '<div class="col-xs-2">할당액</div>'
-                        + '<div class="col-xs-2">내가 낸 금액</div>'
-                        + '<div class="col-xs-2">정산액</div>';
-                }*/
-
                 for(var k=0; k < memberCnt; k++) {
                     if(list[i].purchaser[k].value === Meteor.userId()) {
                         if(searchCategory === 'all'){
@@ -61,22 +51,17 @@ Template.settlementDoIt.onRendered(function() {
                                     var paid = 0;
 
                                     divList += '<div class=row>';
-                                    divList += '<div class=col-xs-1>' + list[i].dateOrdered.substring(5,10) + '</div>';
+                                    divList += '<div class=col-xs-2>' + list[i].dateOrdered.substring(5,10).replace('-', '') + '</div>';
                                     divList += '<div class=col-xs-2>' + list[i].orderSummary + '</div>';
-                                    divList += '<div class=col-xs-2>' + numberWithCommas(list[i].orderPrice) + '</div>';
-                                    divList += '<div class=col-xs-1>' + communalPurchaserCnt + '</div>';
-                                    divList += '<div class=col-xs-2>' + numberWithCommas(sharePrice) + '</div>';
-
-/*                                    trList += '<tr>';
-                                    trList += '<td align=center>' + list[i].dateOrdered + '</td>';
-                                    trList += '<td>' + list[i].orderSummary + '</td>';
-                                    trList += '<td align=right>' + numberWithCommas(list[i].orderPrice) + '</td>';
-                                    trList += '<td align=center>' + communalPurchaserCnt + '</td>';
-                                    trList += '<td align=right>' + numberWithCommas(sharePrice) + '</td>';*/
+                                    divList += '<div class=col-xs-3 style=text-align:right>' + numberWithCommas(list[i].orderPrice) + '</div>';
+                                    divList += '<div class=col-xs-2 style=text-align:right>' + communalPurchaserCnt + '</div>';
+                                    //divList += '<div class=col-xs-2>' + numberWithCommas(sharePrice) + '</div>';
 
                                     for (var y = 0; y < memberCnt; y++) {
                                         if((Meteor.userId() === list[i].purchaser[y].value)
                                             && (list[i].purchaser[y].selected === 'selected')) {
+
+                                            // 한명이 금액을 지불하므로 가격이 내가 낸 금액이 됨.
                                             paid = Number(list[i].orderPrice);
 
                                             for(var z=0; z < memberCnt; z++){
@@ -84,7 +69,7 @@ Template.settlementDoIt.onRendered(function() {
                                                     (list[i].communalPurchaser[z].value !== Meteor.userId())){
                                                     var who = list[i].communalPurchaser[z].value+'_'+list[i].communalPurchaser[z].text;
                                                     if(paidMe.get(who)){
-                                                        var a = Number(paidMe.get(who)) + Number(list[i].communalPurchaser[z].sharePrice);
+                                                        var a = Math.round(Number(paidMe.get(who))) + Math.round(Number(list[i].communalPurchaser[z].sharePrice));
                                                         paidMe.put(who, a);
                                                     }else {
                                                         paidMe.put(who, list[i].communalPurchaser[z].sharePrice)
@@ -96,7 +81,7 @@ Template.settlementDoIt.onRendered(function() {
                                             if(list[i].purchaser[y].selected === 'selected'){
 
                                                 if(paidOther.get(who)){
-                                                    var a = Number(paidOther.get(who)) + Number(list[i].communalPurchaser[y].sharePrice);
+                                                    var a = Math.round(Number(paidOther.get(who))) + Math.round(Number(list[i].communalPurchaser[y].sharePrice));
                                                     paidOther.put(who, a);
                                                 }else {
                                                     paidOther.put(who, list[i].communalPurchaser[y].sharePrice)
@@ -104,17 +89,13 @@ Template.settlementDoIt.onRendered(function() {
                                             }
                                         }
                                     }
+                                    // 정산액 = 할당 액 - 내가 낸 금액
                                     adjustedAmount += (sharePrice - paid);
 
-                                    divList += '<div class=col-xs-2>' + numberWithCommas(paid) + '</div>';
-                                    divList += '<div class=col-xs-2>' + numberWithCommas((sharePrice - paid)).toString() + '</div>';
+                                    divList += '<div class=col-xs-3 style=text-align:right>' + numberWithCommas(paid) + '</div>';
+                                    //divList += '<div class=col-xs-2>' + numberWithCommas((sharePrice - paid)).toString() + '</div>';
                                     divList += '</div>';
                                     divList += '<hr style=border: solid 1px gray;>';
-
-
-/*                                    trList += '<td align=right>' + numberWithCommas(paid) + '</td>';
-                                    trList += '<td class=\'adjust\' align=right>' + numberWithCommas((sharePrice - paid)).toString() + '</td>';
-                                    trList += '</tr>';*/
                                 }
                             }
                         }
@@ -133,22 +114,17 @@ Template.settlementDoIt.onRendered(function() {
                                     var paid = 0;
 
                                     divList += '<div class=row>';
-                                    divList += '<div class=col-xs-1>' + list[i].dateOrdered.substring(5,10) + '</div>';
+                                    divList += '<div class=col-xs-2>' + list[i].dateOrdered.substring(5,10).replace('-','') + '</div>';
                                     divList += '<div class=col-xs-2>' + list[i].orderSummary + '</div>';
-                                    divList += '<div class=col-xs-2>' + numberWithCommas(list[i].orderPrice) + '</div>';
-                                    divList += '<div class=col-xs-1>' + communalPurchaserCnt + '</div>';
-                                    divList += '<div class=col-xs-2>' + numberWithCommas(sharePrice) + '</div>';
-
-                                    /*trList += '<tr>';
-                                    trList += '<td align=center>' + list[i].dateOrdered + '</td>';
-                                    trList += '<td>' + list[i].orderSummary + '</td>';
-                                    trList += '<td align=right>' + numberWithCommas(list[i].orderPrice) + ' 원</td>';
-                                    trList += '<td align=center>' + communalPurchaserCnt + '</td>';
-                                    trList += '<td align=right>' + numberWithCommas(sharePrice) + ' 원</td>';*/
+                                    divList += '<div class=col-xs-3 style=text-align:right>' + numberWithCommas(list[i].orderPrice) + '</div>';
+                                    divList += '<div class=col-xs-2 style=text-align:right>' + communalPurchaserCnt + '</div>';
+                                    //divList += '<div class=col-xs-2>' + numberWithCommas(sharePrice) + '</div>';
 
                                     for (var y = 0; y < memberCnt; y++) {
                                         if((Meteor.userId() === list[i].purchaser[y].value)
                                             && (list[i].purchaser[y].selected === 'selected')) {
+
+                                            // 한명이 금액을 지불하므로 가격이 내가 낸 금액이 됨.
                                             paid = Number(list[i].orderPrice);
 
                                             for(var z=0; z < memberCnt; z++){
@@ -156,7 +132,7 @@ Template.settlementDoIt.onRendered(function() {
                                                     (list[i].communalPurchaser[z].value !== Meteor.userId())){
                                                     var who = list[i].communalPurchaser[z].value+'_'+list[i].communalPurchaser[z].text;
                                                     if(paidMe.get(who)){
-                                                        var a = Number(paidMe.get(who)) + Number(list[i].communalPurchaser[z].sharePrice);
+                                                        var a = Number(paidMe.get(who)) + Math.round(Number(list[i].communalPurchaser[z].sharePrice));
                                                         paidMe.put(who, a);
                                                     }else {
                                                         paidMe.put(who, list[i].communalPurchaser[z].sharePrice)
@@ -168,7 +144,7 @@ Template.settlementDoIt.onRendered(function() {
                                             if(list[i].purchaser[y].selected === 'selected'){
 
                                                 if(paidOther.get(who)){
-                                                    var a = Number(paidOther.get(who)) + Number(list[i].communalPurchaser[y].sharePrice);
+                                                    var a = Number(paidOther.get(who)) + Math.round(Number(list[i].communalPurchaser[y].sharePrice));
                                                     paidOther.put(who, a);
                                                 }else {
                                                     paidOther.put(who, list[i].communalPurchaser[y].sharePrice)
@@ -176,16 +152,14 @@ Template.settlementDoIt.onRendered(function() {
                                             }
                                         }
                                     }
-                                    adjustedAmount += (sharePrice - paid);
 
-                                    divList += '<div class=col-xs-2>' + numberWithCommas(paid) + '</div>';
-                                    divList += '<div class=col-xs-2>' + numberWithCommas((sharePrice - paid)).toString() + '</div>';
+                                    // 정산액 = 할당 액 - 내가 낸 금액
+                                    adjustedAmount += (Math.round(sharePrice) - paid);
+
+                                    divList += '<div class=col-xs-3 style=text-align:right>' + numberWithCommas(paid) + '</div>';
+                                    //divList += '<div class=col-xs-2>' + numberWithCommas((sharePrice - paid)).toString() + '</div>';
                                     divList += '</div>';
                                     divList += '<hr style=border: solid 1px gray;>';
-
-                                    /*trList += '<td align=right>' + numberWithCommas(paid) + ' 원</td>';
-                                    trList += '<td align=right>' + numberWithCommas((sharePrice - paid)).toString() + ' 원</td>';
-                                    trList += '</tr>';*/
                                 }
                             }
                         }
@@ -193,10 +167,6 @@ Template.settlementDoIt.onRendered(function() {
                 }
             }
         }
-
-        /*if(trList === '') {
-            trList = '<tr><td colspan=7 align=center>해당 내역이 없습니다.</td></tr>';
-        }*/
 
         if(divList === '') {
             divList = '<div class=col-xs-12>해당 내역이 없습니다.</div>';
@@ -207,7 +177,7 @@ Template.settlementDoIt.onRendered(function() {
         for(var a in array1){
             var key = array1[a].toString();
             key = key.substring(key.lastIndexOf('_')+1, key.length);
-            paidToMe += key + '에게 ' + numberWithCommas(paidMe.get(array1[a])) + '원 받으세요. <br>';
+            paidToMe += key + '에게 ' + numberWithCommas(Math.round(paidMe.get(array1[a]) * 0.01) * 100) + '원 받으세요. <br>';
         }
 
         var array2 = paidOther.keys();
@@ -215,14 +185,14 @@ Template.settlementDoIt.onRendered(function() {
         for(var a in array2){
             var key = array2[a].toString();
             key = key.substring(key.lastIndexOf('_')+1, key.length);
-            paidToOther += key + '에게 ' + numberWithCommas(paidOther.get(array2[a])) + '원 지불하세요. <br>';
+            paidToOther += key + '에게 ' + numberWithCommas(Math.round(paidOther.get(array2[a]) * 0.01) * 100) + '원 지불하세요. <br>';
         }
 
         if(paidToMe === '') paidToMe = '받을 금액이 없습니다';
         if(paidToOther === '') paidToOther = '지불 할 금액이 없습니다';
 
         this.$('#totalPrice').html(numberWithCommas(totalPrice));
-        this.$('#adjustedAmount').html(numberWithCommas(adjustedAmount).toString());
+        this.$('#adjustedAmount').html(numberWithCommas(Math.round(adjustedAmount * 0.01) * 100).toString());
         this.$('#paidMe').html(paidToMe);
         this.$('#paidOther').html(paidToOther);
         /*this.$('#list').html(trList);*/
